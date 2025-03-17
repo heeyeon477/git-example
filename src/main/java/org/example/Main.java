caa0e95d95de2d45f39117644b7e3fdc84e8115e
heeyeon477 <gimh64627@gmail.com>
2025-03-18 03:48:32 +0900

    git setting 출력문 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,11 @@ package org.example;
 class Main {
     public static void main(String[] args) {
         System.out.println("git setting");
+        System.out.println("git setting");
+        System.out.println("git setting");
+        System.out.println("git setting");
+        System.out.println("git setting");
+        System.out.println("git setting");
+        System.out.println("git setting");
     }
 }
